f2c83cb7c08680476bc5e3c4fa47eab8f21a65b0
balanced-checkpoint-e039 <harmandeepk628@gmail.com>
2021-03-02 04:18:18 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -18,10 +18,12 @@ function ageChecker(age) {
   if (age >=13 && age<= 19){
     return "You are a teenager!"
   }
-  else if (age < 12){
+  else if (age <= 12){
     return "You are a kid"
   }
   else if (age > 19){
     return "You are a grownup"
   }
 }
+
+
